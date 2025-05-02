86ee2e44f1a7b271412e209e2f71f3dd40640a55
MatsudaSyoma <s.matsuda@vr-cnt.com>
2025-05-02 11:34:16 +0900

    UIへの描画できた

--- Source/PixelStreamingClient/Private/CPP_GameMode.cpp
+++ Source/PixelStreamingClient/Private/CPP_GameMode.cpp
@@ -37,14 +37,13 @@ ACPP_GameMode::ACPP_GameMode()
 void ACPP_GameMode::BeginPlay()
 {
     // プレイヤーをスポーン
-	//UCPP_MyGameInstance* gameinstance = Cast<UCPP_MyGameInstance>(GetWorld()->GetGameInstance());
-	//gameinstance->SpawnPlayer(1);
-	
-    // プレイヤーのControllerを取得
-    APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
-    if (PlayerController)
+	UCPP_MyGameInstance* gameinstance = Cast<UCPP_MyGameInstance>(GetWorld()->GetGameInstance());
+	gameinstance->SpawnPlayer(1);
+    TArray<AActor*> FoundCharacters;
+    UGameplayStatics::GetAllActorsOfClass(GetWorld(), ACharacter::StaticClass(), FoundCharacters);
+    for (AActor* Actor : FoundCharacters)
     {
-        ACPP_MyCharacter* PlayerChar = Cast<ACPP_MyCharacter>(PlayerController->GetPawn());
+        ACPP_MyCharacter* PlayerChar = Cast<ACPP_MyCharacter>(Actor);
         if (PlayerChar)
         {
             // PlayerCharにアタッチされているSceneCapture2Dを取得
@@ -70,11 +69,47 @@ void ACPP_GameMode::BeginPlay()
                         // Widgetの関数を呼び出す
                         PlayerView->SetRenderTargetTexture(RenderTarget);
                         PlayerWidget->AddToViewport();
+
                     }
                 }
             }
         }
     }
+    //// プレイヤーのControllerを取得
+    //APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+    //if (PlayerController)
+    //{
+    //    ACPP_MyCharacter* PlayerChar = Cast<ACPP_MyCharacter>(PlayerController->GetPawn());
+    //    if (PlayerChar)
+    //    {
+    //        // PlayerCharにアタッチされているSceneCapture2Dを取得
+    //        USceneCaptureComponent2D* CaptureComp = PlayerChar->PlayerSceneCapture;
+    //        if (CaptureComp)
+    //        {
+    //            // RenderTargetを生成
+    //            UTextureRenderTarget2D* RenderTarget = NewObject<UTextureRenderTarget2D>(this);
+    //            RenderTarget->RenderTargetFormat = RTF_RGBA8;
+    //            RenderTarget->InitAutoFormat(512, 512);
+    //            RenderTarget->UpdateResourceImmediate(true);
+
+    //            // SceneCaptureにRenderTargetを割り当て
+    //            CaptureComp->TextureTarget = RenderTarget;
+
+    //            // 生成したRenderTargetをUIに渡す処理
+    //            UCPP_PlayerView* PlayerWidget = CreateWidget<UCPP_PlayerView>(GetWorld(), PlayerWidgetClass);
+    //            if (PlayerWidget)
+    //            {
+    //                UCPP_PlayerView* PlayerView = Cast<UCPP_PlayerView>(PlayerWidget);
+    //                if (PlayerView)
+    //                {
+    //                    // Widgetの関数を呼び出す
+    //                    PlayerView->SetRenderTargetTexture(RenderTarget);
+    //                    PlayerWidget->AddToViewport();
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
 }
 
 void ACPP_GameMode::PlayerSetup(APlayerController pcontroller)
