46b3251182bffa130de6641d2db5684527ce112a
MatsudaSyoma <s.matsuda@vr-cnt.com>
2025-05-08 17:00:18 +0900

    WebSocketでブラウザからメッセージを送ることができる

--- Source/PixelStreamingClient/Private/CPP_MyGameInstance.cpp
+++ Source/PixelStreamingClient/Private/CPP_MyGameInstance.cpp
@@ -2,9 +2,39 @@
 引数に指定した数を入れるとその数だけスポーンする*/
 
 #include "CPP_MyGameInstance.h"
+#include "WebSocketsModule.h"
+#include "PixelStreamingInputComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 UCPP_MyGameInstance::UCPP_MyGameInstance()
 {
+}
+
+void UCPP_MyGameInstance::Init()
+{
+    Super::Init();
+
+    WebSocket = FWebSocketsModule::Get().CreateWebSocket(TEXT("ws://localhost:8080"));
+
+    WebSocket->OnMessage().AddUObject(this, &UCPP_MyGameInstance::OnMessageReceived);
+    if (WebSocket.IsValid())
+    {
+        WebSocket->Connect();
+        UE_LOG(LogTemp, Log, TEXT("------------------------------WebSocket server started at ws://localhost:8080"));
+    }
+    else
+    {
+        UE_LOG(LogTemp, Error, TEXT("------------------------------Failed to start WebSocket server"));
+    }
+}
+
+void UCPP_MyGameInstance::Shutdown()
+{
+    Super::Shutdown();
+    if (WebSocket.IsValid())
+    {
+        WebSocket->Close();
+    }
 }
 
 // プレイヤーをスポーン
@@ -16,4 +46,9 @@ void UCPP_MyGameInstance::SpawnPlayer(int num)
 		FString outerror;
 		CreateLocalPlayer(-1, outerror, true);
 	}
+}
+
+void UCPP_MyGameInstance::OnMessageReceived(const FString& Message)
+{
+    UE_LOG(LogTemp, Log, TEXT("Received Message: %s"), *Message);
 }
--- Source/PixelStreamingClient/Public/CPP_MyGameInstance.h
+++ Source/PixelStreamingClient/Public/CPP_MyGameInstance.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "IWebSocket.h"
 #include "CPP_MyGameInstance.generated.h"
 
 /**
@@ -16,6 +17,11 @@ class PIXELSTREAMINGCLIENT_API UCPP_MyGameInstance : public UGameInstance
 	
 public:
 	UCPP_MyGameInstance();
+	virtual void Init() override;
+	virtual void Shutdown() override;
 	void SpawnPlayer(int num);
 
+	void OnMessageReceived(const FString& Message);
+
+    TSharedPtr<IWebSocket> WebSocket;
 };
